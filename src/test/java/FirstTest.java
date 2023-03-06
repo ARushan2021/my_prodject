43f921ab10b3b8a54dfd2002bf7b9b86fb2fb53e
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2023-03-06 11:21:35 +0300

    Update FirstTest.java
    2

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -3,7 +3,7 @@ public class FirstTest {
     public static void main(String[] args) {
 
     System.out.println("Приветик");
-        System.out.println("Всем приветики, чтото гит не хочет работать");
+    System.out.println("Всем приветики, чтото гит не хочет работать");
 
     }
 
